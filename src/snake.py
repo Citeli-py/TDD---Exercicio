7926867222888bb83ac605625bf2abe75f8351b1
Citeli-py <matheus.citeli2001@gmail.com>
2025-10-05 17:44:39 -0300

    refactor: direções possiveis sem else if

--- src/snake.py
+++ src/snake.py
@@ -1,32 +1,33 @@
+DIRECTIONS = {
+    "UP": (0, -1),
+    "DOWN": (0, 1),
+    "LEFT": (-1, 0),
+    "RIGHT": (1, 0)
+}
+
 class Snake:
     def __init__(self, initial_position=(0, 0)):
-        self.body = [initial_position]  # lista de tuplas (x,y)
+        self.body = [initial_position]
         self.direction = "UP"
         self.grow_pending = False
 
+    def head(self):
+        return self.body[0]
+
     def change_direction(self, new_direction):
-        # impede que a cobra faça 180° direto
         opposite = {"UP": "DOWN", "DOWN": "UP", "LEFT": "RIGHT", "RIGHT": "LEFT"}
         if opposite[self.direction] != new_direction:
             self.direction = new_direction
 
     def move(self):
-        head_x, head_y = self.body[0]
-        if self.direction == "UP":
-            new_head = (head_x, head_y - 1)
-        elif self.direction == "DOWN":
-            new_head = (head_x, head_y + 1)
-        elif self.direction == "LEFT":
-            new_head = (head_x - 1, head_y)
-        elif self.direction == "RIGHT":
-            new_head = (head_x + 1, head_y)
-
+        dx, dy = DIRECTIONS[self.direction]
+        new_head = (self.head()[0] + dx, self.head()[1] + dy)
         self.body.insert(0, new_head)
 
-        if self.grow_pending:
-            self.grow_pending = False
-        else:
+        if not self.grow_pending:
             self.body.pop()
+        else:
+            self.grow_pending = False
 
     def grow(self):
         self.grow_pending = True
